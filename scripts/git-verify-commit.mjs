84f7a992a0bbb1062d45887f3dea95f4803b613b
yuyi <1138866572@qq.com>
2025-01-04 21:26:53 +0800

    perf: git-hooks verify-commit

--- scripts/git-verify-commit.mjs
+++ scripts/git-verify-commit.mjs
@@ -5,7 +5,7 @@ const msgPath = path.resolve('.git/COMMIT_EDITMSG')
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
 const commitRE =
-  /^(revert: )?(feat|fix|docs|style|refactor|perf|test|chore|types|wip)(\(.+\))?: .{1,50}/
+  /^(revert: )?(feat|fix|docs|style|refactor|perf|test|chore|types)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
   console.log(
