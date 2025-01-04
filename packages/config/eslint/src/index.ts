b898c0eeb5879d8651ab1969c601614d04578033
yuyi <1138866572@qq.com>
2025-01-04 22:28:39 +0800

    chore: @config/eslint update dependencies

--- packages/config/eslint/src/index.ts
+++ packages/config/eslint/src/index.ts
@@ -1,7 +1,6 @@
 import type { Linter } from 'eslint'
 
 import {
-  command,
   ignores,
   javascript,
   jsonc,
@@ -18,7 +17,6 @@ type FlatConfig = Linter.Config
 export async function defineConfig(configs: FlatConfig | FlatConfig[] = []) {
   // 这种包模式部分插件需要异步导入才能使用，如果所有配置都在根目录下的config.js文件下引入则不需要
   const _configs = await Promise.all([
-    command(),
     turbo(),
     jsonc(),
     javascript(),
--- packages/config/eslint/src/plugins/index.ts
+++ packages/config/eslint/src/plugins/index.ts
@@ -1,4 +1,3 @@
-export * from './command.js'
 export * from './ignores.js'
 export * from './javascript.js'
 export * from './jsonc.js'
