03857ff275a6c115e778274c6886163c5fe4276d
yuyi <1138866572@qq.com>
2025-01-13 22:40:57 +0800

    feat: add shadn-ui scroll-area

---
+++ packages/ui/src/shadcn-ui/scroll-area/ScrollArea.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {
+  type ScrollAreaRootProps,
+  ScrollAreaCorner,
+  ScrollAreaRoot,
+  ScrollAreaViewport,
+} from 'radix-vue'
+import { type HTMLAttributes, computed } from 'vue'
+import { cn } from '#/utils'
+import ScrollBar from './ScrollBar.vue'
+
+const props = defineProps<
+  { class?: HTMLAttributes['class'] } & ScrollAreaRootProps
+>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <ScrollAreaRoot
+    v-bind="delegatedProps"
+    :class="cn('relative overflow-hidden', props.class)"
+  >
+    <ScrollAreaViewport class="h-full w-full rounded-[inherit]">
+      <slot></slot>
+    </ScrollAreaViewport>
+    <ScrollBar />
+    <ScrollAreaCorner />
+  </ScrollAreaRoot>
+</template>
